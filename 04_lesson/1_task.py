3932d7cf4d687666d1d60e65ae55ffa6d1c2d1ac
Mariya Lukoyanova <lukoyanovamary@mail.ru>
2023-02-26 16:15:43 +0300

    4 lesson

---
+++ 04_lesson/1_task.py
@@ -0,0 +1,10 @@
+# Напишите программу, которая принимает на вход строку, и отслеживает, 
+# сколько раз каждый символ уже встречался. Количество повторов добавляется к
+# символам с помощью постфикса формата _n
+# a a a b c a a d c d d
+string = input('Enter string: ').split()
+dict = {}.fromkeys(string, 0)
+for i in string:
+    print(f"{i}_{dict[i]}" if dict[i] else i, end =" ")
+    dict[i]+=1
+
